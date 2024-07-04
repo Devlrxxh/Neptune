c81f20b39d72dcd63ad3af56f916f9ce7949f354
devlrxxh <seabearvalorant@gmail.com>
2024-07-04 20:15:32 +0200

    Unfinished Sync plugin

--- Sync/src/main/java/me/dev/lrxh/sync/Sync.java
+++ Sync/src/main/java/me/dev/lrxh/sync/Sync.java
@@ -1,17 +1,22 @@
 package me.dev.lrxh.sync;
 
+import lombok.Getter;
+import me.dev.lrxh.sync.game.GameServerManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
 public final class Sync extends JavaPlugin {
+    private static Sync instance;
+    private GameServerManager gameServerManager;
+
+    public static Sync get() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        gameServerManager = new GameServerManager();
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
