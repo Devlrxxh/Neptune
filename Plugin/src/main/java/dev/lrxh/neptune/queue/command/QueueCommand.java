d234d22729a4dc88e487ea049c1dbddefa02b86d
Deniz USTA <ddenizustaa@outlook.com.tr>
2024-08-24 20:15:33 +0300

    Fixed queueing in match

--- Plugin/src/main/java/dev/lrxh/neptune/queue/command/QueueCommand.java
+++ Plugin/src/main/java/dev/lrxh/neptune/queue/command/QueueCommand.java
@@ -4,8 +4,11 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import dev.lrxh.neptune.Neptune;
 import dev.lrxh.neptune.kit.Kit;
+import dev.lrxh.neptune.profile.data.ProfileState;
+import dev.lrxh.neptune.profile.impl.Profile;
 import dev.lrxh.neptune.queue.Queue;
 import dev.lrxh.neptune.queue.menu.QueueMenu;
+import dev.lrxh.neptune.utils.CC;
 import org.bukkit.entity.Player;
 
 @CommandAlias("queue")
@@ -17,6 +20,14 @@ public class QueueCommand extends BaseCommand {
     @Syntax("<kit>")
     @CommandCompletion("@kits")
     public void queue(Player player, String kitName) {
+
+        Profile userProfile = plugin.getAPI().getProfile(player);
+
+        if(userProfile.getState().equals(ProfileState.IN_GAME)) {
+            player.sendMessage(CC.error("You can't queue right now!"));
+            return;
+        }
+
         if (plugin.getKitManager().getKitByName(kitName) != null) {
             Queue queue = new Queue(plugin.getKitManager().getKitByName(kitName));
             plugin.getQueueManager().add(player.getUniqueId(), queue);
