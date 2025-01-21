cb4b3dfb112c7e23e9a11e8f19f82878210c51a1
Devlrxxh <seabearvalorant@gmail.com>
2025-01-21 10:14:07 +0100

    Improved block tracker code

--- Plugin/src/main/java/dev/lrxh/neptune/match/listener/BlockTracker.java
+++ Plugin/src/main/java/dev/lrxh/neptune/match/listener/BlockTracker.java
@@ -6,9 +6,7 @@ import dev.lrxh.neptune.profile.impl.Profile;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.EnderCrystal;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -26,62 +24,47 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 public class BlockTracker implements Listener {
 
-    private final Map<UUID, Entity> crystalOwners = new HashMap<>();
+    private final Map<UUID, EnderCrystal> crystalOwners = new HashMap<>();
+
+    private Optional<Match> getMatchForPlayer(Player player) {
+        Profile profile = API.getProfile(player);
+        return Optional.ofNullable(profile)
+                .map(Profile::getMatch);
+    }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
-        Profile profile = API.getProfile(player);
-        Match match = profile.getMatch();
-        if (match == null) return;
-
-        match.getChanges().put(event.getBlock().getLocation(), event.getBlockReplacedState().getBlockData());
+        getMatchForPlayer(player).ifPresent(match -> {
+            match.getChanges().put(event.getBlock().getLocation(), event.getBlockReplacedState().getBlockData());
+        });
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onCrystalPlace(EntitySpawnEvent event) {
-        Entity entity = event.getEntity();
-        if (!(entity instanceof EnderCrystal)) return;
-        CreatureSpawnEvent.SpawnReason spawnReason = entity.getEntitySpawnReason();
-        if (!spawnReason.equals(CreatureSpawnEvent.SpawnReason.DEFAULT)) return;
+        if (!(event.getEntity() instanceof EnderCrystal crystal)) return;
 
-        Player player = entity.getWorld().getPlayers().stream()
-                .filter(p -> p.getLocation().distance(entity.getLocation()) < 5)
+        if (!event.getEntity().getEntitySpawnReason().equals(CreatureSpawnEvent.SpawnReason.DEFAULT)) return;
+
+        Player player = crystal.getWorld().getPlayers().stream()
+                .filter(p -> p.getLocation().distance(crystal.getLocation()) < 5)
                 .findFirst()
                 .orElse(null);
         if (player == null) return;
 
-        Profile profile = API.getProfile(player);
-        Match match = profile.getMatch();
-        if (match == null) return;
-
-        match.getEntities().add(entity);
+        getMatchForPlayer(player).ifPresent(match -> match.getEntities().add(crystal));
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onEntityDamage(EntityDamageByEntityEvent event) {
         if (event.getEntity() instanceof EnderCrystal && event.getDamager() instanceof Player player) {
-            crystalOwners.put(player.getUniqueId(), event.getEntity());
+            crystalOwners.put(player.getUniqueId(), (EnderCrystal) event.getEntity());
         }
-    }
-
-    private Player getPlayer(EnderCrystal entity) {
-        for (Map.Entry<UUID, Entity> entry : crystalOwners.entrySet()) {
-            if (entry.getValue().equals(entity)) {
-                Player player = Bukkit.getPlayer(entry.getKey());
-                if (player == null) {
-                    crystalOwners.remove(entry.getKey());
-                    continue;
-                }
-
-                return player;
-            }
-        }
-        return null;
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
@@ -92,26 +75,28 @@ public class BlockTracker implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onEndCrystalExplosion(EntityExplodeEvent event) {
         if (!(event.getEntity() instanceof EnderCrystal endCrystal)) return;
-        Player player = getPlayer(endCrystal);
+
+        Player player = crystalOwners.get(endCrystal.getUniqueId()) != null
+                ? Bukkit.getPlayer(crystalOwners.get(endCrystal.getUniqueId()).getUniqueId()) : null;
+
         if (player == null) return;
-        Profile profile = API.getProfile(player);
-        Match match = profile.getMatch();
-        if (match == null) return;
-        for (Block block : event.blockList()) {
-            block.getDrops().clear();
-            match.getChanges().put(block.getLocation(), block.getBlockData());
-        }
+
+        getMatchForPlayer(player).ifPresent(match -> {
+            for (Block block : event.blockList()) {
+                block.getDrops().clear();
+                match.getChanges().put(block.getLocation(), block.getBlockData());
+            }
+        });
+
         crystalOwners.remove(player.getUniqueId());
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBucketEmpty(PlayerBucketEmptyEvent event) {
         Player player = event.getPlayer();
-        Profile profile = API.getProfile(player);
-        Match match = profile.getMatch();
-        if (match == null) return;
-
-        match.getLiquids().add(event.getBlock().getLocation());
+        getMatchForPlayer(player).ifPresent(match -> {
+            match.getLiquids().add(event.getBlock().getLocation());
+        });
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
@@ -122,22 +107,20 @@ public class BlockTracker implements Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
-        Profile profile = API.getProfile(player);
-        Match match = profile.getMatch();
-        if (match == null) return;
-        if (match.getPlacedBlocks().contains(event.getBlock().getLocation())) return;
-
-        match.getChanges().put(event.getBlock().getLocation(), event.getBlock().getBlockData());
+        getMatchForPlayer(player).ifPresent(match -> {
+            if (!match.getPlacedBlocks().contains(event.getBlock().getLocation())) {
+                match.getChanges().put(event.getBlock().getLocation(), event.getBlock().getBlockData());
+            }
+        });
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onMultiPlace(BlockMultiPlaceEvent event) {
         Player player = event.getPlayer();
-        Profile profile = API.getProfile(player);
-        Match match = profile.getMatch();
-        if (match == null) return;
-        for (BlockState blockState : event.getReplacedBlockStates()) {
-            match.getChanges().put(blockState.getLocation(), blockState.getBlockData());
-        }
+        getMatchForPlayer(player).ifPresent(match -> {
+            for (BlockState blockState : event.getReplacedBlockStates()) {
+                match.getChanges().put(blockState.getLocation(), blockState.getBlockData());
+            }
+        });
     }
 }
