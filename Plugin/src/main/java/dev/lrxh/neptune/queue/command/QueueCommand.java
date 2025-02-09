cc46f40dbb149f361b27197bbc4c8c53db1c2627
Devlrxxh <seabearvalorant@gmail.com>
2025-02-09 16:16:32 +0100

    Added /queue menu

--- Plugin/src/main/java/dev/lrxh/neptune/queue/command/QueueCommand.java
+++ Plugin/src/main/java/dev/lrxh/neptune/queue/command/QueueCommand.java
@@ -6,9 +6,15 @@ import com.jonahseguin.drink.annotation.Sender;
 import dev.lrxh.neptune.kit.Kit;
 import dev.lrxh.neptune.queue.Queue;
 import dev.lrxh.neptune.queue.QueueService;
+import dev.lrxh.neptune.queue.menu.QueueMenu;
 import org.bukkit.entity.Player;
 
 public class QueueCommand {
+
+    @Command(name = "menu ", desc = "")
+    public void menu(@Sender Player player) {
+        new QueueMenu().open(player);
+    }
 
     @Command(name = "", desc = "", usage = "<kit>")
     public void queue(@Sender Player player, Kit kit) {
