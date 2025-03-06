ab0f6302536f24ad41399ea4341917f2079dc034
Devlrxxh <seabearvalorant@gmail.com>
2025-03-06 11:38:28 +0100

    Fixed play again item being given to non duel matches

--- Plugin/src/main/java/dev/lrxh/neptune/match/impl/FfaFightMatch.java
+++ Plugin/src/main/java/dev/lrxh/neptune/match/impl/FfaFightMatch.java
@@ -3,7 +3,6 @@ package dev.lrxh.neptune.match.impl;
 import dev.lrxh.neptune.API;
 import dev.lrxh.neptune.arena.Arena;
 import dev.lrxh.neptune.configs.impl.MessagesLocale;
-import dev.lrxh.neptune.hotbar.HotbarService;
 import dev.lrxh.neptune.kit.Kit;
 import dev.lrxh.neptune.match.Match;
 import dev.lrxh.neptune.match.impl.participant.DeathCause;
@@ -33,8 +32,6 @@ public class FfaFightMatch extends Match {
     public void end(Participant loser) {
         state = MatchState.ENDING;
         loser.setLoser(true);
-
-        forEachPlayer(player -> HotbarService.get().giveItems(player));
 
         forEachParticipant(participant -> {
             if (winner == null) return;
