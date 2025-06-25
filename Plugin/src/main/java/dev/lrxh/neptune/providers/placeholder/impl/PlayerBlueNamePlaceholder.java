25e8133501b2aff3f6ac192065813b79f6155b53
mallusrgreatv2 <mallusrgreat@gmail.com>
2025-06-25 20:19:21 +0530

    Remove spectator check

--- Plugin/src/main/java/dev/lrxh/neptune/providers/placeholder/impl/PlayerBlueNamePlaceholder.java
+++ Plugin/src/main/java/dev/lrxh/neptune/providers/placeholder/impl/PlayerBlueNamePlaceholder.java
@@ -3,7 +3,6 @@ package dev.lrxh.neptune.providers.placeholder.impl;
 import dev.lrxh.neptune.API;
 import dev.lrxh.neptune.game.match.Match;
 import dev.lrxh.neptune.game.match.impl.solo.SoloFightMatch;
-import dev.lrxh.neptune.profile.data.ProfileState;
 import dev.lrxh.neptune.profile.impl.Profile;
 import dev.lrxh.neptune.providers.placeholder.Placeholder;
 import org.bukkit.OfflinePlayer;
@@ -15,7 +14,7 @@ public class PlayerBlueNamePlaceholder implements Placeholder {
         if (profile == null) return string;
         Match match = profile.getMatch();
         if (string.equals("player-blue-name")) {
-          if(profile.getState() != ProfileState.IN_SPECTATOR || match == null || !(match instanceof SoloFightMatch soloFightMatch)) return "";
+          if(match == null || !(match instanceof SoloFightMatch soloFightMatch)) return "";
           return soloFightMatch.getParticipantB().getNameUnColored();
         }
 
