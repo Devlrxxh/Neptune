0f1ba009fa21f4813e15dd41d3fbcd364a905a75
mallusrgreatv2 <mallusrgreat@duck.com>
2025-06-28 18:02:31 +0530

    Use PaginatedMenu for party duel menu

--- Plugin/src/main/java/dev/lrxh/neptune/feature/party/menu/buttons/events/PartyDuelMenu.java
+++ Plugin/src/main/java/dev/lrxh/neptune/feature/party/menu/buttons/events/PartyDuelMenu.java
@@ -5,13 +5,13 @@ import dev.lrxh.neptune.feature.party.Party;
 import dev.lrxh.neptune.feature.party.PartyService;
 import dev.lrxh.neptune.utils.menu.Button;
 import dev.lrxh.neptune.utils.menu.Filter;
-import dev.lrxh.neptune.utils.menu.Menu;
+import dev.lrxh.neptune.utils.menu.PaginatedMenu;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PartyDuelMenu extends Menu {
+public class PartyDuelMenu extends PaginatedMenu {
     private final Party party;
 
     public PartyDuelMenu(Party party) {
@@ -20,7 +20,7 @@ public class PartyDuelMenu extends Menu {
     }
 
     @Override
-    public List<Button> getButtons(Player player) {
+    public List<Button> getAllPagesButtons(Player player) {
         List<Button> buttons = new ArrayList<>();
         int i = 0;
         for (Party p : PartyService.get().getParties().stream().filter(p -> !p.equals(party)).toList()) {
