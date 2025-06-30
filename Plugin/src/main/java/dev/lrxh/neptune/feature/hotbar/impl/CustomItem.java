de73f50d680be5692b0135d75ef0bfb103eea39a
HeadsShot <41201881+Starfruit2210@users.noreply.github.com>
2025-07-01 01:45:17 +0700

    add: lore support towards Hotbar

--- Plugin/src/main/java/dev/lrxh/neptune/feature/hotbar/impl/CustomItem.java
+++ Plugin/src/main/java/dev/lrxh/neptune/feature/hotbar/impl/CustomItem.java
@@ -2,12 +2,14 @@ package dev.lrxh.neptune.feature.hotbar.impl;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class CustomItem extends Item {
     private final String command;
 
-    public CustomItem(String displayName, String material, byte slot, String command) {
-        super(null, displayName, material, true, slot);
+    public CustomItem(String displayName, String material, List<String> lore, byte slot, String command) {
+        super(null, displayName, material, lore, true, slot);
         this.command = command;
     }
 }
