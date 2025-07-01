de0fce60ae1a28687be49df77bfb60ba24ff7317
Devlrxxh <seabearvalorant@gmail.com>
2025-07-01 06:47:41 +0200

    Make all menus open synchronous

--- Plugin/src/main/java/dev/lrxh/neptune/utils/menu/Menu.java
+++ Plugin/src/main/java/dev/lrxh/neptune/utils/menu/Menu.java
@@ -1,5 +1,6 @@
 package dev.lrxh.neptune.utils.menu;
 
+import dev.lrxh.neptune.Neptune;
 import dev.lrxh.neptune.configs.impl.MenusLocale;
 import dev.lrxh.neptune.utils.CC;
 import dev.lrxh.neptune.utils.ServerUtils;
@@ -55,52 +56,54 @@ public abstract class Menu {
     }
 
     public void open(Player player) {
-        String title;
-        if (this.title.isEmpty()) {
-            title = getTitle(player);
-        } else {
-            title = this.title;
-        }
-
-        Inventory inventory = Bukkit.createInventory(player, size, CC.color(title));
-
-        buttons = getButtons(player);
-        switch (filter) {
-            case FILL -> {
-                for (int i = 0; i < inventory.getSize(); i++) {
-                    if (getButton(i) == null) {
-                        buttons.add(new DisplayButton(i, Material.getMaterial(MenusLocale.FILTER_MATERIAL.getString()), MenusLocale.FILTER_NAME.getString()));
-                    }
-                }
+        Bukkit.getScheduler().runTask(Neptune.get(), () -> {
+            String title;
+            if (this.title.isEmpty()) {
+                title = getTitle(player);
+            } else {
+                title = this.title;
             }
-            case BORDER -> {
-                int rows = size / 9;
-                int columns = 9;
 
-                for (int i = 0; i < size; i++) {
-                    int row = i / columns;
-                    int column = i % columns;
+            Inventory inventory = Bukkit.createInventory(player, size, CC.color(title));
 
-                    if (row == 0 || row == rows - 1 || column == 0 || column == columns - 1) {
+            buttons = getButtons(player);
+            switch (filter) {
+                case FILL -> {
+                    for (int i = 0; i < inventory.getSize(); i++) {
                         if (getButton(i) == null) {
                             buttons.add(new DisplayButton(i, Material.getMaterial(MenusLocale.FILTER_MATERIAL.getString()), MenusLocale.FILTER_NAME.getString()));
                         }
                     }
                 }
+                case BORDER -> {
+                    int rows = size / 9;
+                    int columns = 9;
+
+                    for (int i = 0; i < size; i++) {
+                        int row = i / columns;
+                        int column = i % columns;
+
+                        if (row == 0 || row == rows - 1 || column == 0 || column == columns - 1) {
+                            if (getButton(i) == null) {
+                                buttons.add(new DisplayButton(i, Material.getMaterial(MenusLocale.FILTER_MATERIAL.getString()), MenusLocale.FILTER_NAME.getString()));
+                            }
+                        }
+                    }
+                }
+                case NONE -> {
+                }
             }
-            case NONE -> {
+
+
+            for (Button button : buttons) {
+                set(inventory, button.getSlot(), button.getItemStack(player));
             }
-        }
 
+            player.openInventory(inventory);
+            player.updateInventory();
 
-        for (Button button : buttons) {
-            set(inventory, button.getSlot(), button.getItemStack(player));
-        }
-
-        player.openInventory(inventory);
-        player.updateInventory();
-
-        MenuService.get().add(player, this);
+            MenuService.get().add(player, this);
+        });
     }
 
     public Button getButton(int slot) {
