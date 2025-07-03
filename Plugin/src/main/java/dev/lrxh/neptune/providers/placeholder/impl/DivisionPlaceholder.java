2e19c7f4be34abc8a9741afc5b8b694817148866
Devlrxxh <seabearvalorant@gmail.com>
2025-07-03 14:36:48 +0200

    Added Global Division

---
+++ Plugin/src/main/java/dev/lrxh/neptune/providers/placeholder/impl/DivisionPlaceholder.java
@@ -0,0 +1,22 @@
+package dev.lrxh.neptune.providers.placeholder.impl;
+
+import dev.lrxh.neptune.API;
+import dev.lrxh.neptune.profile.data.GlobalStats;
+import dev.lrxh.neptune.profile.impl.Profile;
+import dev.lrxh.neptune.providers.placeholder.Placeholder;
+import org.bukkit.OfflinePlayer;
+
+public class DivisionPlaceholder implements Placeholder {
+    @Override
+    public String parse(OfflinePlayer player, String string) {
+        Profile profile = API.getProfile(player);
+        if (profile == null) return string;
+        GlobalStats globalStats = profile.getGameData().getGlobalStats();
+
+        if (string.equals("division")) {
+            return String.valueOf(globalStats.getDivision().getDisplayName());
+        }
+
+        return string;
+    }
+}
