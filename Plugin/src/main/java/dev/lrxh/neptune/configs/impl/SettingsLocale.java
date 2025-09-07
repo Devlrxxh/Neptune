fbc4eb5d45db4c97b619a17a5bd9157879c03276
mallusrgreatv2 <mallusrgreat@duck.com>
2025-09-07 19:24:49 +0530

    Add friendly fire setting

--- Plugin/src/main/java/dev/lrxh/neptune/configs/impl/SettingsLocale.java
+++ Plugin/src/main/java/dev/lrxh/neptune/configs/impl/SettingsLocale.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @Getter
 public enum SettingsLocale implements IDataAccessor {
+    FRIENDLY_FIRE("FRIENDLY_FIRE", DataType.BOOLEAN, "false"),
     COMMANDS_AFTER_MATCH_WINNER("COMMAND_AFTER_MATCH.WINNER", DataType.STRING_LIST, "NONE"),
     COMMANDS_AFTER_MATCH_LOSER("COMMAND_AFTER_MATCH.LOSER", DataType.STRING_LIST, "NONE"),
     SPAWN_LOCATION("SPAWN.LOCATION", DataType.STRING, "NONE"),
