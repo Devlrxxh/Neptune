702b64e02e901fda2f1c30722cdab12f42017567
Devlrxxh <seabearvalorant@gmail.com>
2025-09-13 10:36:31 +0200

    Added <kit> placeholder for KIT_EDITOR_SELECT_LORE lore

--- Plugin/src/main/java/dev/lrxh/neptune/game/kit/menu/editor/button/KitEditorSelectButton.java
+++ Plugin/src/main/java/dev/lrxh/neptune/game/kit/menu/editor/button/KitEditorSelectButton.java
@@ -8,6 +8,7 @@ import dev.lrxh.neptune.profile.data.ProfileState;
 import dev.lrxh.neptune.profile.impl.Profile;
 import dev.lrxh.neptune.providers.clickable.Replacement;
 import dev.lrxh.neptune.utils.ItemBuilder;
+import dev.lrxh.neptune.utils.ItemUtils;
 import dev.lrxh.neptune.utils.menu.Button;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
@@ -39,7 +40,7 @@ public class KitEditorSelectButton extends Button {
     @Override
     public ItemStack getItemStack(Player player) {
         return new ItemBuilder(kit.getIcon()).name(MenusLocale.KIT_EDITOR_SELECT_KIT_NAME.getString().replace("<kit>", kit.getDisplayName()))
-                .lore(MenusLocale.KIT_EDITOR_SELECT_LORE.getStringList(), player)
+                .lore(ItemUtils.getLore(MenusLocale.KIT_EDITOR_SELECT_LORE.getStringList(), new Replacement("<kit>", kit.getDisplayName())), player)
                 .build();
     }
 }
