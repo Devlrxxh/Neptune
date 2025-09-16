9917d0f2c806ae2129e156ec13664c1761f6a6ce
Devlrxxh <seabearvalorant@gmail.com>
2025-09-16 18:52:41 +0200

    Added arena recapture button

--- Plugin/src/main/java/dev/lrxh/neptune/game/arena/menu/ArenaManagementMenu.java
+++ Plugin/src/main/java/dev/lrxh/neptune/game/arena/menu/ArenaManagementMenu.java
@@ -1,9 +1,11 @@
 package dev.lrxh.neptune.game.arena.menu;
 
+import dev.lrxh.blockChanger.snapshot.CuboidSnapshot;
 import dev.lrxh.neptune.game.arena.Arena;
 import dev.lrxh.neptune.game.arena.impl.EdgeType;
 import dev.lrxh.neptune.game.arena.menu.button.*;
 import dev.lrxh.neptune.game.match.impl.participant.ParticipantColor;
+import dev.lrxh.neptune.utils.ItemBuilder;
 import dev.lrxh.neptune.utils.menu.Button;
 import dev.lrxh.neptune.utils.menu.Filter;
 import dev.lrxh.neptune.utils.menu.Menu;
@@ -11,6 +13,8 @@ import dev.lrxh.neptune.utils.menu.impl.DisplayButton;
 import dev.lrxh.neptune.utils.menu.impl.ReturnButton;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,11 +49,25 @@ public class ArenaManagementMenu extends Menu {
 
             buttons.add(new ArenaSetLimitButton(4, arena));
 
-            buttons.add(new DisplayButton(getSize() - 5, Material.GRASS_BLOCK, "&aManage Whitelisted Blocks", o -> {
-                new WhitelistedBlocksMenu(arena).open(player);
-            }));
+            buttons.add(new DisplayButton(getSize() - 5, Material.GRASS_BLOCK, "&aManage Whitelisted Blocks", o -> new WhitelistedBlocksMenu(arena).open(player)));
             buttons.add(new ArenaSetEdgeButton(8, arena, EdgeType.MAX));
             buttons.add(new ArenaSetEdgeButton(7, arena, EdgeType.MIN));
+
+            buttons.add(new Button(31) {
+                @Override
+                public ItemStack getItemStack(Player player) {
+                    return new ItemBuilder(Material.EMERALD).name("&aRecapture Arena").build();
+                }
+
+                @Override
+                public void onClick(ClickType type, Player player) {
+                    arena.setDoneLoading(false);
+                    CuboidSnapshot.create(arena.getMin(), arena.getMax()).thenAccept(snapshot -> {
+                        arena.setSnapshot(snapshot);
+                        arena.setDoneLoading(true);
+                    });
+                }
+            });
         }
 
         buttons.add(new ReturnButton(getSize() - 9, new ArenasManagementMenu()));
