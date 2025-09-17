29cff8e2f7c968760783cc35d1bf10925118e41f
OceanTw <windgame999@gmail.com>
2025-09-16 21:12:27 -0500

    Ignore bucket empty event cancellation if player is in custom state

--- Plugin/src/main/java/dev/lrxh/neptune/game/match/listener/MatchListener.java
+++ Plugin/src/main/java/dev/lrxh/neptune/game/match/listener/MatchListener.java
@@ -503,6 +503,8 @@ public class MatchListener implements Listener {
         if (player.getGameMode().equals(GameMode.CREATIVE))
             return;
 
+        if (API.getProfile(player).getState().equals(ProfileState.IN_CUSTOM)) return;
+
         Optional<Profile> profileOpt = getMatchProfile(player);
         if (!profileOpt.isPresent()) {
             event.setCancelled(true);
